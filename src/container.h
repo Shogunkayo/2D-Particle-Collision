d6bac7ef146541dce1454658466e079173b9a0e4
pYr0 <pyrotourface@gmail.com>
2024-05-10 15:57:24 +0530

    made container boundary public

--- src/container.h
+++ src/container.h
@@ -12,12 +12,13 @@ struct ContainerBoundary {
 
 class Container {
 private:
-    struct ContainerBoundary boundary;
     Shader shader;
     unsigned int VBO;
     unsigned int VAO;
 
 public:
+    struct ContainerBoundary boundary;
+
     Container(const float width, const float height, const float scr_width, const float scr_height, const std::string& filepath, const glm::vec4& color);
     ~Container();
 
